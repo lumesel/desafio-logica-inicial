ffa6d1ca1095cc37150e3d9990b6944964be9937
lumesel <luisa_mesel@hotmail.com>
2025-08-11 22:33:41 -0300

    desafiojavaoficial
    Desafio do herói - Felipão

--- desafiojava04.js
+++ desafiojava04.js
@@ -9,23 +9,23 @@ console.log(`${username} iniciou sua jornada com ${xp} de xp, no nível ${levelI
 function getLevel(){
     xp+=500
 
-if (xp<=1000){
+    if (xp<=1000){
         level = "Ferro"
-} else if (xp <= 2000){
+    } else if (xp <= 2000){
         level = "Bronze"
-} else if (xp <= 5000){
+    } else if (xp <= 5000){
         level = "Prata"
-} else if (xp <= 7000){
+    } else if (xp <= 7000){
         level = "Ouro"
-} else if (xp <= 8000){
+    } else if (xp <= 8000){
         level = "Platina"
-} else if (xp <= 9000){
+    } else if (xp <= 9000){
         level = "Ascendente"
-} else if (xp <= 10000){
+    } else if (xp <= 10000){
         level = "Imortal"
-} else if (xp > 10000){
+    } else if (xp > 10000){
         level = "Radiante"
-}
+    }
 }
 
 while (xp<10500){
